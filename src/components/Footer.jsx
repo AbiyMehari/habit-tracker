9ea35815be4a51c5703aed051848e95a4595d376
Abiy Alem <188307829+AbiyMehari@users.noreply.github.com>
2025-06-04 12:07:02 +0200

    some corrections

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-8 py-4 text-center text-gray-500 text-sm">
+    <footer className="mt-8 py-4 text-center text-sm text-gray-500">
       <p>
         &copy; {new Date().getFullYear()} Habit Tracker by Abiy - Built with
         React , Vite & Tailwind CSS
