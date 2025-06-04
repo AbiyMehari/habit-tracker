50d117e4ef6e8bb198f1f3b0e067b956b179582e
Abiy Alem <188307829+AbiyMehari@users.noreply.github.com>
2025-06-04 07:48:16 +0200

    copy right added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-blue-100 text-center py-4 mt-10 text-sm text-gray-600">
-      <p>Made with 💙 by Abiy | {new Date().getFullYear()}</p>
+      <p>&copy; Abiy | {new Date().getFullYear()}</p>
     </footer>
   );
 }
