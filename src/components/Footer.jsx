a6b9f7d3a2e68be4b7b86868ede57a30660cac81
Abiy Alem <188307829+AbiyMehari@users.noreply.github.com>
2025-06-04 12:14:43 +0200

    re-do all the conntent

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,7 @@
 export default function Footer() {
   return (
     <footer className="mt-8 py-4 text-center text-sm text-gray-500">
-      <p>
-        &copy; {new Date().getFullYear()} Habit Tracker by Abiy - Built with
-        React , Vite & Tailwind CSS
-      </p>
+      <p>© {new Date().getFullYear()} Habit Tracker - Built with React</p>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-blue-600 text-white p-4 shadow-md">
-      <h1 className="text-2xl font-bold">Habit Tracker App</h1>
+      <h1 className="text-2xl font-bold">Habit Tracker</h1>
       <p className="text-sm opacity-80">
         Track your daily habits and build a better you!
       </p>
